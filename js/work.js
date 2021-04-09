a65808e4de640d4f7ab3041e36124d66a2001150
julienzbinden <80947080+julienzbinden@users.noreply.github.com>
2021-04-09 18:58:23 +0200

    v.0.3.0.4

--- js/work.js
+++ js/work.js
@@ -13,11 +13,11 @@ function display() {
     list.innerHTML = "";
 
     for (var i = 0; i < projects_json.projects.length; i++) {
-        var link = projects_json.projects[i].name.toLowerCase().replace(/ /gi,'-');
+        var link = projects_json.projects[i].name.toLowerCase().replace(/ /gi,'-').replace(/'/gi,'');
         var name = projects_json.projects[i].name;
         var date = projects_json.projects[i].date;
         var text = projects_json.projects[i].text;
 
-        list.insertAdjacentHTML('beforeend','<section class="project"><a class="link" href="' + link + '"></a><div class="project-box"><p class="date">' + date + '</p><h1 class="title">' + name + '</h1><div class="link-icon"></div><p class="text">' + text + '</p><div class="line-vertical"></div><div class="line-vertical"></div><div class="line-horizontal"></div></div><div class="image" style="background-image: url("https://julienzbinden.github.io/css/img/work/' + link + '");"></div></section>');
+        list.insertAdjacentHTML('beforeend','<section class="project"><a class="link" href="' + link + '"></a><div class="project-box"><p class="date">' + date + '</p><h1 class="title">' + name + '</h1><div class="link-icon"></div><p class="text">' + text + '</p><div class="line-vertical"></div><div class="line-vertical"></div><div class="line-horizontal"></div></div><div class="image" style="background-image: url(https:\/\/julienzbinden.github.io\/css\/img\/work\/' + link + ');"></div></section>');
     }
 }
