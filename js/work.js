13f33092efb8883f11ca3555db351aad3674ec75
julienzbinden <80947080+julienzbinden@users.noreply.github.com>
2021-04-09 11:28:20 +0200

    v.0.3.0.1

--- js/work.js
+++ js/work.js
@@ -12,11 +12,11 @@ function display() {
     var list = document.querySelector('.content');
     list.innerHTML = "";
 
-    for (var i = 0; i < projects_json.projects.length; i++) {
-        var link = projects_json.projects[0].name.toLowercase.replace(" ", "-");
-        var name = projects_json.projects[0].name;
-        var date = projects_json.projects[0].date;
-        var text = projects_json.projects[0].text;
+    for (var i = 0; i < projects_json.projects[].length; i++) {
+        var link = projects_json.projects[i].name.toLowercase.replace(" ", "-");
+        var name = projects_json.projects[i].name;
+        var date = projects_json.projects[i].date;
+        var text = projects_json.projects[i].text;
 
         list.insertAdjacentHTML('afterend','<section class="project"><a class="link" href="' + link + '"></a><div class="project-box"><p class="date">' + date + '</p><h1 class="title">' + name + '</h1><div class="link-icon"></div><p class="text">' + text + '</p><div class="line-vertical"></div><div class="line-vertical"></div><div class="line-horizontal"></div></div><div class="image" style="background-image: url("https://julienzbinden.github.io/css/img/work/' + link + '");"></div></section>');
     }
